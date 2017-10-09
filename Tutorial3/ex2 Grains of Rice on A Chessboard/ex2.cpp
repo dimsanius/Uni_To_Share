dff290e46161bde6943a8154f3a6bc1d9f5cb2b7
santarod <santarod@uni.coventry.ac.uk>
2017-10-09 15:54:22 +0100

    ex2 done, need to redo the folders T_T

---
+++ Tutorial3/ex2 Grains of Rice on A Chessboard/ex2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip> 
+
+using namespace std;
+
+int main()
+{
+	double rice_everyday = 1;
+	double total_rice = rice_everyday;
+	int counter = 1;
+	while (counter <= 64)
+	{
+		cout << "Day " << counter << ": " << fixed << setprecision (0) << rice_everyday << " rice today, " << fixed << setprecision(0) << total_rice << " total rice so far." << endl;
+		rice_everyday *= 2;
+		total_rice += rice_everyday;
+		counter++;
+	}
+	system("pause");
+	return 0;
+}
