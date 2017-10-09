59239b51587498e0062dbfd43bc320876ac0b5bc
santarod <santarod@uni.coventry.ac.uk>
2017-10-09 17:32:24 +0100

    redone tut2 2ex in proper format

---
+++ Tutorial2/Ex2 Input/Ex2_code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//Activity 2
+	cout << "---------Activity 2---------\n";
+	int var;
+	cout << "Now, input value of variable: ";
+	cin >> var;
+	cout << "Well done! You input " << var;
+	cout << "\n";
+	system("pause");
+	return 0;
+}
