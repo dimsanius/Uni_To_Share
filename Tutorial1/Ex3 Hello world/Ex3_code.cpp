aa0e52696db04c3cea7ca81e3010bf09a3fce27b
santarod <santarod@uni.coventry.ac.uk>
2017-10-10 16:25:31 +0100

    added endl and made it more fancy

--- Tutorial1/Ex3 Hello world/Ex3_code.cpp
+++ Tutorial1/Ex3 Hello world/Ex3_code.cpp
@@ -4,6 +4,7 @@ using namespace std;					//using std that includes main operations
 
 int main()								//start of main function
 {
-	cout << "Hello world!";				//printing hello world into console line
+	cout << "Hello world!" << endl;		//printing hello world into console line and ending line after it
+	system("pause");					//pausing execution of an app to avoid app closing
 	return 0;							//finishing ecxecuting app. Passing value 0 into main function
 }
