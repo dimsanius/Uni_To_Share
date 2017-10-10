a497cd07938c1a40d17fb8efe54330331cf6d3b8
santarod <santarod@uni.coventry.ac.uk>
2017-10-10 16:23:36 +0100

    adding first tutorial excercises

---
+++ Tutorial1/Ex3 Hello world/Ex3_code.cpp
@@ -0,0 +1,9 @@
+#include <iostream>						//including iostream library into application. Helps printing out text
+
+using namespace std;					//using std that includes main operations
+
+int main()								//start of main function
+{
+	cout << "Hello world!";				//printing hello world into console line
+	return 0;							//finishing ecxecuting app. Passing value 0 into main function
+}
