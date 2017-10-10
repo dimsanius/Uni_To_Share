a73eb30d36b3c73ec6b901772c364e64213eab2c
santarod <santarod@uni.coventry.ac.uk>
2017-10-10 16:47:13 +0100

    Ascii art added

---
+++ Tutorial1/Ex4 ASCII art challenge/ex4_code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	printf("  ___ ____ ____  __ ____      __  __ ____ _   \n / __(  _ (  __)/ _(_  _)   _(  )/  (  _ / \\  \n( (_ \\)   /) _)/    \\)(    / \\) (  O ) _ \\_/  \n \\___(__\\_(____\\_/\\_(__)   \\____/\\__(____(_)  \n");
+	//special character cant be printed the right way so had to escape the sequence
+	system("pause");
+	return 0;
+}
