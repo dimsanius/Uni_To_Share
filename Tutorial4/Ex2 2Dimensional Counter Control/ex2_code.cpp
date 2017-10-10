7bd301c220c80fb7ba3cbc2feafd094fc4eab79b
santarod <santarod@uni.coventry.ac.uk>
2017-10-10 11:56:07 +0100

    tut4 ex2 corrected to follow exercise example

--- Tutorial4/Ex2 2Dimensional Counter Control/ex2_code.cpp
+++ Tutorial4/Ex2 2Dimensional Counter Control/ex2_code.cpp
@@ -8,11 +8,6 @@ int main()
 {
 	for (int i = 1; i <= 20; i++)
 	{
-		if (i == 1)
-			cout << " " << "\t";
-		else
-			cout  << setw(4) << i << "\t";
-
 		for (int y = 1; y <= 20; y++)
 		{
 			cout << setw(4) << i*y << " ";
