38dc59dafcadaa96f0481d0a2e4c8a09dddb6aaa
santarod <santarod@uni.coventry.ac.uk>
2017-10-16 12:29:40 +0100

    ex2 done. Added very hard level

--- Tutorial4/Hw2 Pancake Glutton/hw2_code.cpp
+++ Tutorial4/Hw2 Pancake Glutton/hw2_code.cpp
@@ -13,10 +13,10 @@ int main()
 	int personMostPancakes;
 	int personLeastPancakes;
 
-	int personMostPancakesVH[10] = {};
-	int personLeastPancakesVH[10] = {};
-	int pancakesEatenVH[10] = {};
-	
+	int PANCAKES_EATEN[10] = {};
+	int biggestAmount = -1;
+	int PERSON_INDEX_WITH_BIGGEST_AMOUNT[10] = {};
+	int amountOfPeopleWithBiggestAmountEaten = 0;
 
 	while (true)
 	{
@@ -28,6 +28,7 @@ int main()
 		switch (choice)
 		{
 		case 1:
+			//Easy level
 			for (int i = 1; i <= 10; i++)
 			{
 
@@ -47,6 +48,7 @@ int main()
 			break;
 
 		case 2:
+			//Medium level
 			for (int i = 1; i <= 10; i++)
 			{
 
@@ -72,6 +74,7 @@ int main()
 			break;
 
 		case 3:
+			//Hard level
 			for (int i = 1; i <= 10; i++)
 			{
 
@@ -109,36 +112,43 @@ int main()
 			break;
 
 		case 4: 
+			//Very hard level
 			for (int i = 0; i < 10; i++)
 			{
+				//Getting all values of eaten pancakes and finding the biggest amount eaten
+				cout << "How many pancakes were eaten by person " << i+1 << ": ";
+				cin >> PANCAKES_EATEN[i];	
 
-				cout << "How many pancakes were eaten by person " << i << ": ";
-				cin >> pancakesEatenVH[i];
+				if (biggestAmount < PANCAKES_EATEN[i])
+					biggestAmount = PANCAKES_EATEN[i];
+			}
 
-
-				if (i == 1)
+			for (int i = 0; i < 10; i++)
+			{
+				//finding indexes and amount of people who ate the most pancakes
+				if (PANCAKES_EATEN[i] == biggestAmount)
 				{
-					//primary assignition
-					mostPanckaesEaten = pancakesEatenVH[i];
-					leastPanckaesEaten = pancakesEatenVH[i];
-					personMostPancakesVH[i] = i;
-					personLeastPancakesVH[i] = i;
-
+					PERSON_INDEX_WITH_BIGGEST_AMOUNT[amountOfPeopleWithBiggestAmountEaten] = i;
+					amountOfPeopleWithBiggestAmountEaten++;
 				}
-
-				if (mostPanckaesEaten < pancakesEaten)
-					mostPanckaesEaten = pancakesEaten;
-			
-
-				if (leastPanckaesEaten > pancakesEaten)
-					leastPanckaesEaten = pancakesEaten;
-			
-
 			}
 
 
-			cout << "The biggest amount of pancakes eaten: " << mostPanckaesEaten << ", and was eaten by " << personMostPancakes << "." << endl;
-			cout << "The least amount of pancakes eaten: " << leastPanckaesEaten << ", and was eaten by " << personLeastPancakes << "." << endl;
+			for (int i = 0; i < amountOfPeopleWithBiggestAmountEaten; i++)
+			{
+				//Outputting acquired information beautifully
+				if(i == 0)
+					cout << "Person " << PERSON_INDEX_WITH_BIGGEST_AMOUNT[i] + 1;
+				else if (i < amountOfPeopleWithBiggestAmountEaten - 1)
+					cout << ", Person " << PERSON_INDEX_WITH_BIGGEST_AMOUNT[i] + 1;
+				else
+					cout << " and Person " << PERSON_INDEX_WITH_BIGGEST_AMOUNT[i] + 1;
+
+			}
+			if(amountOfPeopleWithBiggestAmountEaten > 1)
+				cout << " ate the most pancakes: " << biggestAmount << " each!" << endl;
+			else
+				cout << " ate the most pancakes: " << biggestAmount << endl;
 
 			break;
 
