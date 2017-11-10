aeb55584a531b164e005d4488825362ec2c4e0b9
santarod <santarod@uni.coventry.ac.uk>
2017-11-10 11:46:37 +0000

    real and imaginary part done

--- Coursework/DFT/cw_code.cpp
+++ Coursework/DFT/cw_code.cpp
@@ -4,27 +4,27 @@
 #include <fstream>
 #include "DFTCore.h"
 
+
+
 using namespace std;
-/*
-struct polar
-{
-	double degrees = 0, length = -0.5000;
-};
 
-struct cart
-{
-	double realPart, imaginaryPart;
-	char symbol;
-};
-*/
 
+const double PI = 3.14159265358979323846;
 struct polarData
 {
 	double inDegrees, inLength;
 };
+
+struct DFTCoefficients
+{
+	double realPart=0, imaginaryPart=0;
+};
 int main()
 {
+	cout << fixed << setprecision(4);
+
 	polarData polar[361];
+	DFTCoefficients DFTCoef;
 	DFTCore DFT[8];
 
 	cout << DFTCore::getAmountOfObjects() << endl;
@@ -33,21 +33,11 @@ int main()
 	{
 		inStream >> polar[i].inDegrees;
 		inStream >> polar[i].inLength;
+		DFTCoef.realPart += (polar[i].inLength*PI / 180)*cos(i*PI / 180);
+		DFTCoef.imaginaryPart += (polar[i].inLength*PI / 180)*sin(i*PI / 180);
 	}
-	//cout << polar[360].inDegrees;
-	/*
-	struct polar polarData;
-	struct cart cartData;
-	cartData.realPart = polarData.length * cos(polarData.degrees);
-	cartData.imaginaryPart = polarData.length * sin(polarData.degrees);
-	if (cartData.imaginaryPart >= (int)0)
-		cartData.symbol = '+';
-	else
-		cartData.symbol = '-';
-	cout << "Input polar form: " << polarData.length << ", degrees = " << polarData.degrees << endl;
-	cout << "Resulting cartesean formula is " << setprecision(4) << cartData.realPart << ' ' << cartData.symbol << ' ' << cartData.imaginaryPart << "i" << endl;
-	*/
-
+	cout << DFTCoef.realPart << endl;
+	cout << DFTCoef.imaginaryPart << endl;
 
 	
 	system("pause");
